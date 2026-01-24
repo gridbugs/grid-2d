b02e7bb23633250ae053111d7f8b2f3843399709
Stephen Sherratt <stephen@sherra.tt>
2026-01-25 01:26:13 +1100

    Update cord_2d

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-pub use coord_2d::{self, Coord, Size};
+pub use coord_2d::{self, ICoord, UCoord};
 #[cfg(feature = "serialize")]
 use serde::{Deserialize, Serialize};
 use std::iter;
@@ -7,21 +7,21 @@ use std::ops::{Index, IndexMut};
 use std::slice;
 use std::vec;
 
-pub type CoordIter = coord_2d::CoordIterRowMajor;
-pub type EdgeCoordIter = coord_2d::EdgeIter;
+pub type ICoordIter = coord_2d::ICoordIterRowMajor;
+pub type EdgeICoordIter = coord_2d::EdgeIter;
 pub type GridIter<'a, T> = slice::Iter<'a, T>;
 pub type GridIterMut<'a, T> = slice::IterMut<'a, T>;
-pub type GridEnumerate<'a, T> = iter::Zip<CoordIter, GridIter<'a, T>>;
-pub type GridEnumerateMut<'a, T> = iter::Zip<CoordIter, GridIterMut<'a, T>>;
+pub type GridEnumerate<'a, T> = iter::Zip<ICoordIter, GridIter<'a, T>>;
+pub type GridEnumerateMut<'a, T> = iter::Zip<ICoordIter, GridIterMut<'a, T>>;
 pub type GridIntoIter<T> = vec::IntoIter<T>;
-pub type GridIntoEnumerate<T> = iter::Zip<CoordIter, GridIntoIter<T>>;
+pub type GridIntoEnumerate<T> = iter::Zip<ICoordIter, GridIntoIter<T>>;
 pub type GridRows<'a, T> = slice::Chunks<'a, T>;
 pub type GridRowsMut<'a, T> = slice::ChunksMut<'a, T>;
-pub type GridEdgeEnumerate<'a, T> = iter::Zip<EdgeCoordIter, GridEdgeIter<'a, T>>;
-pub type GridEdgeEnumerateMut<'a, T> = iter::Zip<EdgeCoordIter, GridEdgeIterMut<'a, T>>;
+pub type GridEdgeEnumerate<'a, T> = iter::Zip<EdgeICoordIter, GridEdgeIter<'a, T>>;
+pub type GridEdgeEnumerateMut<'a, T> = iter::Zip<EdgeICoordIter, GridEdgeIterMut<'a, T>>;
 
 #[derive(Debug, Clone, Copy)]
-pub struct IteratorLengthDifferentFromSize;
+pub struct IteratorLengthDifferentFromUCoord;
 
 #[derive(Debug)]
 pub enum Get2Error {
@@ -34,37 +34,37 @@ pub enum Get2Error {
 #[derive(Debug, Clone, Hash, PartialOrd, Ord, PartialEq, Eq)]
 pub struct Grid<T> {
     cells: Vec<T>,
-    size: Size,
+    size: UCoord,
 }
 
 impl<T> Grid<T> {
-    pub fn new_fn<F>(size: Size, mut f: F) -> Self
+    pub fn new_fn<F>(size: UCoord, mut f: F) -> Self
     where
-        F: FnMut(Coord) -> T,
+        F: FnMut(ICoord) -> T,
     {
         let count = size.count();
         let mut cells = Vec::with_capacity(count);
-        for coord in size.coord_iter_row_major() {
+        for coord in size.icoord_iter_row_major() {
             cells.push(f(coord));
         }
         Self { cells, size }
     }
 
     pub fn try_new_iterator<I>(
-        size: Size,
+        size: UCoord,
         iterator: I,
-    ) -> Result<Self, IteratorLengthDifferentFromSize>
+    ) -> Result<Self, IteratorLengthDifferentFromUCoord>
     where
         I: Iterator<Item = T>,
     {
         let cells: Vec<T> = iterator.collect();
         if cells.len() != size.count() {
-            return Err(IteratorLengthDifferentFromSize);
+            return Err(IteratorLengthDifferentFromUCoord);
         }
         Ok(Self { cells, size })
     }
 
-    pub fn new_iterator<I>(size: Size, iterator: I) -> Self
+    pub fn new_iterator<I>(size: UCoord, iterator: I) -> Self
     where
         I: Iterator<Item = T>,
     {
@@ -82,12 +82,12 @@ impl<T> Grid<T> {
 
     pub fn new_grid_map_with_coord<U, F>(grid: Grid<U>, mut f: F) -> Self
     where
-        F: FnMut(Coord, U) -> T,
+        F: FnMut(ICoord, U) -> T,
     {
         let size = grid.size;
         let cells = size
-            .coord_iter_row_major()
-            .zip(grid.cells.into_iter())
+            .icoord_iter_row_major()
+            .zip(grid.cells)
             .map(|(coord, u)| f(coord, u))
             .collect();
         Self { cells, size }
@@ -104,7 +104,7 @@ impl<T> Grid<T> {
 
     pub fn new_grid_map_ref_with_coord<U, F>(grid: &Grid<U>, mut f: F) -> Self
     where
-        F: FnMut(Coord, &U) -> T,
+        F: FnMut(ICoord, &U) -> T,
     {
         let cells = grid
             .coord_iter()
@@ -119,7 +119,7 @@ impl<T> Grid<T> {
 }
 
 impl<T: Clone> Grid<T> {
-    pub fn new_clone(size: Size, value: T) -> Self {
+    pub fn new_clone(size: UCoord, value: T) -> Self {
         let count = size.count();
         let mut cells = Vec::with_capacity(count);
         cells.resize(count, value);
@@ -134,7 +134,7 @@ impl<T: Clone> Grid<T> {
 }
 
 impl<T: Copy> Grid<T> {
-    pub fn new_copy(size: Size, value: T) -> Self {
+    pub fn new_copy(size: UCoord, value: T) -> Self {
         let count = size.count();
         let mut cells = Vec::with_capacity(count);
         cells.resize_with(count, || value);
@@ -143,7 +143,7 @@ impl<T: Copy> Grid<T> {
 }
 
 impl<T: Default> Grid<T> {
-    pub fn new_default(size: Size) -> Self {
+    pub fn new_default(size: UCoord) -> Self {
         let count = size.count();
         let mut cells = Vec::new();
         cells.resize_with(count, Default::default);
@@ -158,53 +158,56 @@ impl<T> Grid<T> {
     pub fn height(&self) -> u32 {
         self.size.height()
     }
-    pub fn size(&self) -> Size {
+    pub fn size(&self) -> UCoord {
         self.size
+    }
+    pub fn is_empty(&self) -> bool {
+        self.size.is_empty()
     }
     pub fn len(&self) -> usize {
         self.cells.len()
     }
-    pub fn iter(&self) -> GridIter<T> {
+    pub fn iter(&self) -> GridIter<'_, T> {
         self.cells.iter()
     }
-    pub fn iter_mut(&mut self) -> GridIterMut<T> {
+    pub fn iter_mut(&mut self) -> GridIterMut<'_, T> {
         self.cells.iter_mut()
     }
-    pub fn coord_iter(&self) -> CoordIter {
-        self.size.coord_iter_row_major()
+    pub fn coord_iter(&self) -> ICoordIter {
+        self.size.icoord_iter_row_major()
     }
-    pub fn get(&self, coord: Coord) -> Option<&T> {
+    pub fn get(&self, coord: ICoord) -> Option<&T> {
         self.index_of_coord(coord).map(|index| &self.cells[index])
     }
-    pub fn get_mut(&mut self, coord: Coord) -> Option<&mut T> {
+    pub fn get_mut(&mut self, coord: ICoord) -> Option<&mut T> {
         self.index_of_coord(coord)
             .map(move |index| &mut self.cells[index])
     }
-    pub fn get_tiled(&self, coord: Coord) -> &T {
+    pub fn get_tiled(&self, coord: ICoord) -> &T {
         &self.cells[self.index_of_normalized_coord(coord)]
     }
-    pub fn get_tiled_mut(&mut self, coord: Coord) -> &mut T {
+    pub fn get_tiled_mut(&mut self, coord: ICoord) -> &mut T {
         let index = self.index_of_normalized_coord(coord);
         &mut self.cells[index]
     }
-    pub fn index_of_coord_unchecked(&self, coord: Coord) -> usize {
+    pub fn index_of_coord_unchecked(&self, coord: ICoord) -> usize {
         (coord.y as u32 * self.size.width() + coord.x as u32) as usize
     }
-    pub fn index_of_coord(&self, coord: Coord) -> Option<usize> {
+    pub fn index_of_coord(&self, coord: ICoord) -> Option<usize> {
         if coord.is_valid(self.size) {
             Some(self.index_of_coord_unchecked(coord))
         } else {
             None
         }
     }
-    fn index_of_coord_checked(&self, coord: Coord) -> usize {
+    fn index_of_coord_checked(&self, coord: ICoord) -> usize {
         if coord.is_valid(self.size) {
             self.index_of_coord_unchecked(coord)
         } else {
             panic!("coord out of bounds");
         }
     }
-    fn index_of_normalized_coord(&self, coord: Coord) -> usize {
+    fn index_of_normalized_coord(&self, coord: ICoord) -> usize {
         self.index_of_coord_unchecked(coord.normalize(self.size))
     }
     pub fn get_index_checked(&self, index: usize) -> &T {
@@ -213,29 +216,29 @@ impl<T> Grid<T> {
     pub fn get_index_checked_mut(&mut self, index: usize) -> &mut T {
         self.cells.index_mut(index)
     }
-    pub fn get_checked(&self, coord: Coord) -> &T {
+    pub fn get_checked(&self, coord: ICoord) -> &T {
         self.cells.index(self.index_of_coord_checked(coord))
     }
-    pub fn get_checked_mut(&mut self, coord: Coord) -> &mut T {
+    pub fn get_checked_mut(&mut self, coord: ICoord) -> &mut T {
         let index = self.index_of_coord_checked(coord);
         self.cells.index_mut(index)
     }
-    pub fn enumerate(&self) -> GridEnumerate<T> {
+    pub fn enumerate(&self) -> GridEnumerate<'_, T> {
         self.coord_iter().zip(self.iter())
     }
-    pub fn enumerate_mut(&mut self) -> GridEnumerateMut<T> {
+    pub fn enumerate_mut(&mut self) -> GridEnumerateMut<'_, T> {
         self.coord_iter().zip(self.iter_mut())
     }
     pub fn into_enumerate(self) -> GridIntoEnumerate<T> {
-        self.coord_iter().zip(self.cells.into_iter())
+        self.coord_iter().zip(self.cells)
     }
-    pub fn rows(&self) -> GridRows<T> {
+    pub fn rows(&self) -> GridRows<'_, T> {
         self.cells.chunks(self.size.width() as usize)
     }
-    pub fn rows_mut(&mut self) -> GridRowsMut<T> {
+    pub fn rows_mut(&mut self) -> GridRowsMut<'_, T> {
         self.cells.chunks_mut(self.size.width() as usize)
     }
-    pub fn get2_mut(&mut self, a: Coord, b: Coord) -> Result<(&mut T, &mut T), Get2Error> {
+    pub fn get2_mut(&mut self, a: ICoord, b: ICoord) -> Result<(&mut T, &mut T), Get2Error> {
         if a == b {
             return Err(Get2Error::CoordsEqual);
         }
@@ -249,7 +252,7 @@ impl<T> Grid<T> {
             Ok((&mut slice_a[0], &mut slice_b[index_b]))
         }
     }
-    pub fn get2_checked_mut(&mut self, a: Coord, b: Coord) -> (&mut T, &mut T) {
+    pub fn get2_checked_mut(&mut self, a: ICoord, b: ICoord) -> (&mut T, &mut T) {
         if a == b {
             panic!("coords may not be equal");
         }
@@ -272,50 +275,50 @@ impl<T> Grid<T> {
     pub fn map<U, F: FnMut(T) -> U>(self, f: F) -> Grid<U> {
         Grid::new_grid_map(self, f)
     }
-    pub fn map_with_coord<U, F: FnMut(Coord, T) -> U>(self, f: F) -> Grid<U> {
+    pub fn map_with_coord<U, F: FnMut(ICoord, T) -> U>(self, f: F) -> Grid<U> {
         Grid::new_grid_map_with_coord(self, f)
     }
     pub fn map_ref<U, F: FnMut(&T) -> U>(&self, f: F) -> Grid<U> {
         Grid::new_grid_map_ref(self, f)
     }
-    pub fn map_ref_with_coord<U, F: FnMut(Coord, &T) -> U>(&self, f: F) -> Grid<U> {
+    pub fn map_ref_with_coord<U, F: FnMut(ICoord, &T) -> U>(&self, f: F) -> Grid<U> {
         Grid::new_grid_map_ref_with_coord(self, f)
     }
-    pub fn is_on_edge(&self, coord: Coord) -> bool {
+    pub fn is_on_edge(&self, coord: ICoord) -> bool {
         self.size.is_on_edge(coord)
     }
-    pub fn edge_coord_iter(&self) -> EdgeCoordIter {
+    pub fn edge_coord_iter(&self) -> EdgeICoordIter {
         self.size.edge_iter()
     }
-    pub fn edge_iter(&self) -> GridEdgeIter<T> {
+    pub fn edge_iter(&self) -> GridEdgeIter<'_, T> {
         GridEdgeIter {
             edge_coord_iter: self.edge_coord_iter(),
             grid: self,
         }
     }
-    pub fn edge_iter_mut(&mut self) -> GridEdgeIterMut<T> {
+    pub fn edge_iter_mut(&mut self) -> GridEdgeIterMut<'_, T> {
         GridEdgeIterMut {
             edge_coord_iter: self.edge_coord_iter(),
             grid: self,
             marker: PhantomData,
         }
     }
-    pub fn edge_enumerate(&self) -> GridEdgeEnumerate<T> {
+    pub fn edge_enumerate(&self) -> GridEdgeEnumerate<'_, T> {
         self.edge_coord_iter().zip(self.edge_iter())
     }
-    pub fn edge_enumerate_mut(&mut self) -> GridEdgeEnumerateMut<T> {
+    pub fn edge_enumerate_mut(&mut self) -> GridEdgeEnumerateMut<'_, T> {
         self.edge_coord_iter().zip(self.edge_iter_mut())
     }
 }
 
 pub struct GridEdgeIter<'a, T> {
-    edge_coord_iter: EdgeCoordIter,
+    edge_coord_iter: EdgeICoordIter,
     grid: &'a Grid<T>,
 }
 
 impl<'a, T> Iterator for GridEdgeIter<'a, T> {
     type Item = &'a T;
-    fn next<'b>(&'b mut self) -> Option<Self::Item> {
+    fn next(&mut self) -> Option<Self::Item> {
         self.edge_coord_iter
             .next()
             .map(|coord| self.grid.get_checked(coord))
@@ -323,7 +326,7 @@ impl<'a, T> Iterator for GridEdgeIter<'a, T> {
 }
 
 pub struct GridEdgeIterMut<'a, T> {
-    edge_coord_iter: EdgeCoordIter,
+    edge_coord_iter: EdgeICoordIter,
     grid: *mut Grid<T>,
     marker: PhantomData<&'a mut T>,
 }
@@ -332,7 +335,7 @@ impl<'a, T> Iterator for GridEdgeIterMut<'a, T> {
     type Item = &'a mut T;
     fn next(&mut self) -> Option<Self::Item> {
         if let Some(coord) = self.edge_coord_iter.next() {
-            // SAFETY: The compiler doesn't know that the `EdgeCoordIter` iterator only visits each
+            // SAFETY: The compiler doesn't know that the `EdgeICoordIter` iterator only visits each
             // coordinate at most once. If this iterator yielded the same coordinate twice, then
             // the `EdgeIterMut` iterator could give out multiple mutable references to the same
             // cell of the grid.
@@ -349,28 +352,31 @@ mod tests {
     use super::*;
     use std::mem;
 
-    fn coord_grid(size: Size) -> Grid<Coord> {
+    fn coord_grid(size: UCoord) -> Grid<ICoord> {
         Grid::new_fn(size, |coord| coord)
     }
 
     #[test]
     #[should_panic]
     fn out_of_bounds() {
-        let grid = coord_grid(Size::new(2, 3));
-        grid.get_checked(Coord::new(0, 3));
+        let grid = coord_grid(UCoord::new(2, 3));
+        grid.get_checked(ICoord::new(0, 3));
     }
 
     #[test]
     fn tiling() {
-        let mut grid = coord_grid(Size::new(2, 3));
-        assert_eq!(*grid.get_tiled(Coord::new(-10, -30)), Coord::new(0, 0));
-        *grid.get_tiled_mut(Coord::new(-12, -12)) = Coord::new(1000, 1000);
-        assert_eq!(*grid.get_tiled(Coord::new(10, 30)), Coord::new(1000, 1000));
+        let mut grid = coord_grid(UCoord::new(2, 3));
+        assert_eq!(*grid.get_tiled(ICoord::new(-10, -30)), ICoord::new(0, 0));
+        *grid.get_tiled_mut(ICoord::new(-12, -12)) = ICoord::new(1000, 1000);
+        assert_eq!(
+            *grid.get_tiled(ICoord::new(10, 30)),
+            ICoord::new(1000, 1000)
+        );
     }
 
     #[test]
     fn enumerate() {
-        let mut grid = coord_grid(Size::new(24, 42));
+        let mut grid = coord_grid(UCoord::new(24, 42));
         grid.enumerate()
             .for_each(|(coord, cell)| assert_eq!(coord, *cell));
         grid.enumerate_mut()
@@ -381,26 +387,26 @@ mod tests {
 
     #[test]
     fn index() {
-        let mut grid = coord_grid(Size::new(7, 9));
-        let index = grid.index_of_coord(Coord::new(5, 3)).unwrap();
+        let mut grid = coord_grid(UCoord::new(7, 9));
+        let index = grid.index_of_coord(ICoord::new(5, 3)).unwrap();
         assert_eq!(index, 26);
-        *grid.get_index_checked_mut(index) = Coord::new(1000, 1000);
-        assert_eq!(*grid.get_index_checked(index), Coord::new(1000, 1000));
+        *grid.get_index_checked_mut(index) = ICoord::new(1000, 1000);
+        assert_eq!(*grid.get_index_checked(index), ICoord::new(1000, 1000));
     }
 
     #[test]
     fn get2_checked_mut() {
-        let mut grid = coord_grid(Size::new(4, 4));
-        let (a, b) = grid.get2_checked_mut(Coord::new(1, 2), Coord::new(2, 1));
+        let mut grid = coord_grid(UCoord::new(4, 4));
+        let (a, b) = grid.get2_checked_mut(ICoord::new(1, 2), ICoord::new(2, 1));
         mem::swap(a, b);
-        assert_eq!(*grid.get_checked(Coord::new(1, 2)), Coord::new(2, 1));
-        assert_eq!(*grid.get_checked(Coord::new(2, 1)), Coord::new(1, 2));
+        assert_eq!(*grid.get_checked(ICoord::new(1, 2)), ICoord::new(2, 1));
+        assert_eq!(*grid.get_checked(ICoord::new(2, 1)), ICoord::new(1, 2));
     }
 
     #[test]
     #[should_panic]
     fn get2_checked_mut_equal_coords() {
-        let mut grid = coord_grid(Size::new(4, 4));
-        let (_, _) = grid.get2_checked_mut(Coord::new(1, 2), Coord::new(1, 2));
+        let mut grid = coord_grid(UCoord::new(4, 4));
+        let (_, _) = grid.get2_checked_mut(ICoord::new(1, 2), ICoord::new(1, 2));
     }
 }
